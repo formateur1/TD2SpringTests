5930f8b32c8dc17f5bfee7e757f56c96fdd627e0
formateur1 <enseignant.formateur@protonmail.com>
2022-05-25 16:42:12 +0200

    Utilisation de la base H2 Réalisation des tests des opérations CRUD pour SalarieRepository

--- src/main/java/com/inti/TD2SpringTests/repository/SalarieRepository.java
+++ src/main/java/com/inti/TD2SpringTests/repository/SalarieRepository.java
@@ -1,11 +1,14 @@
 package com.inti.TD2SpringTests.repository;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.inti.TD2SpringTests.model.Salarie;
 
 @Repository
+//@Transactional
 public interface SalarieRepository extends JpaRepository<Salarie, Integer>
 {
 
--- src/test/java/com/inti/TD2SpringTests/reposiroty/SalarieRepositoryTests.java
+++ src/test/java/com/inti/TD2SpringTests/reposiroty/SalarieRepositoryTests.java
@@ -2,33 +2,46 @@ package com.inti.TD2SpringTests.reposiroty;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.inti.TD2SpringTests.model.Salarie;
 import com.inti.TD2SpringTests.repository.SalarieRepository;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class SalarieRepositoryTests
 {
 	
 	@Autowired
 	SalarieRepository salarieRepository;
 	
-	Salarie salarie, salarie2;
+	Salarie salarie, salarie2, salarie1;
 	
 	@BeforeEach
 	public void setUp()
 	{
 		salarie = new Salarie();
 		salarie2 = new Salarie("Toto", "Titi", "tt@gmail.com");
+		salarie1 = salarieRepository.save(salarie2);
 	}
 	
 	@Test
@@ -43,4 +56,103 @@ public class SalarieRepositoryTests
 		assertThat(savedSalarie).isNotNull();
 		assertThat(savedSalarie.getId()).isGreaterThan(1);
 	}
+	
+	@Test
+	public void TestGetSalarie()
+	{
+		//When  (recupère en base)
+		Salarie toGetSalarie = salarieRepository.getReferenceById(2);
+		
+		//Then
+		assertThat(toGetSalarie).isNotNull();
+		assertThat(toGetSalarie.getId()).isEqualTo(2);
+		assertThat(toGetSalarie.getNom()).isEqualTo("Sophie");
+		assertThat(toGetSalarie.getPrenom()).isEqualTo("Marie");
+		assertThat(toGetSalarie.getEmail()).isEqualTo("sm@gmail.com");
+	}
+	
+	@Test
+//	@Rollback(false)
+//	@Commit
+	public void TestDeleteExceptionSalarie()
+	{
+		// Given		
+//		Salarie salarie = salarieRepository.save(salarie2);
+//		int id = salarie.getId();
+//		System.out.println("salarié : " + salarie);
+//		
+//		// When		
+//		salarieRepository.deleteById(id);
+//		salarie = salarieRepository.getReferenceById(id);
+//		System.out.println("salarié : " + salarie);
+		
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {salarieRepository.deleteById(1);});
+		
+		// Then
+//		assertThat(salarie).isNull();
+	}
+	
+	@Test
+	@Transactional(value = TxType.NEVER)
+	public void TestDeleteSalarie()
+	{
+		try {
+		// Given		
+		
+		int id = salarie1.getId();
+		System.out.println("salarié : " + salarie1);
+		
+		Salarie s1 = salarieRepository.getReferenceById(id);
+		assertThat(s1).isNotNull();
+		
+		// When		
+		salarieRepository.delete(s1);
+		salarie1 = salarieRepository.getReferenceById(id);
+		System.out.println("salarié : " + salarie1);
+		
+		// Then
+		assertThat(salarie1).isNull();
+		}
+		catch (Exception e) {
+			e.getStackTrace();
+			// TODO: handle exception
+		}
+	}
+	
+	@Test
+	public void testUpdateSalarie()
+	{
+		// given
+		Salarie s1 = salarieRepository.save(salarie2);
+		
+		// when
+		s1.setNom("Ronaldo");
+		s1.setPrenom("Cristiano");
+		Salarie s2 = salarieRepository.save(s1);
+		
+		// then
+		assertThat(s2).isNotNull();
+		assertThat(s2.getNom()).isEqualTo("Ronaldo");
+	}
+	
+	@Test
+	public void testGetAllSalarie()
+	{
+		// given
+		Salarie s1 = salarieRepository.save(new Salarie("Jean", "Claude", "jc@gmail.com"));
+		Salarie s2 = salarieRepository.save(new Salarie("Louis", "Claude", "lc@gmail.com"));
+		
+		// when
+		List<Salarie> lisSalaries = salarieRepository.findAll();
+		
+		// then
+		assertThat(lisSalaries).isNotEmpty();
+		assertThat(lisSalaries).hasSize(3);
+		assertThat(lisSalaries.get(0).getClass()).hasSameClassAs(Salarie.class);
+		assertThat(lisSalaries.get(1).toString()).hasToString(s1.toString());
+		
+	}
+	
+	
+	
 }
